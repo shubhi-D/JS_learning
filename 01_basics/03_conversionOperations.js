704051bc54d14816815f2552c859a288c75c3b22
shubhi-D <115149360+shubhi-D@users.noreply.github.com>
2024-04-10 19:40:20 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -18,8 +18,44 @@ let inBool=Boolean(isLog)
 console.log(inBool)*/
 
 
-let someNumber=33
+/*let someNumber=33
 let strNum=String(someNumber)
 console.log(strNum)
-console.log(typeof strNum)
+console.log(typeof strNum)*/
+
+// ***************OPERATIONS******************** //
+
+let value=3
+let negValue = -value
+//console.log(negValue)
+
+/*console.log(2+2)
+console.log(2-2)
+console.log(2/2)*/
+
+let str1="hello"
+let str2="   shubhi"
+
+let str3= str1+str2
+console.log(str3)
+
+console.log("1"+2)//12
+console.log("1"+"2")//12
+console.log(1+"2")//12
+console.log("1"+2+2)//122
+console.log(1+2+"3")//33
+
+console.log(+true)
+console.log(+false)
+console.log(+"")
+//console.log(true+)//syntax error : unexpected token ')'
+
+let n1,n2,n3
+n1=n2=n3=2+2
+console.log(n1)
+
+let gameCounter=100
+//gameCounter++
+++gameCounter
+console.log(gameCounter)
 
