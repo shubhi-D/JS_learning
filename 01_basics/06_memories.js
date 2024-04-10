1021d530da4c4a73ecce29839bb2f6bf70415fa4
shubhi-D <115149360+shubhi-D@users.noreply.github.com>
2024-04-10 20:33:24 +0000

    memory concept

---
+++ 01_basics/06_memories.js
@@ -0,0 +1,12 @@
+/* two types of memory-
+     stack(primitive)
+     heap(non-premitive)
+ */
+
+     let myName="kalimbaGrooves"
+     let anotherName=myName
+
+     anotherName="shubhi-D"
+     console.log(myName)
+     console.log(anotherName)
+     console.log(anotherName)
