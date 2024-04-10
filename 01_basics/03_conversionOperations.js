a1c07802c8b19f8b4369abaf93558df50356bfd1
shubhi-D <115149360+shubhi-D@users.noreply.github.com>
2024-04-10 19:19:40 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score="33abc"
+
+/*console.log(typeof score);//string
+console.log(typeof(score)); //string
+
+let valInNumber=Number(score) // convert
+console.log(typeof valInNumber);//number
+console.log(valInNumber); //NaN
+*/
+/*
+when we convert
+
+"33"-> 33
+"33acs"->NaN
+*/
+/*let isLog=1
+let inBool=Boolean(isLog)
+console.log(inBool)*/
+
+
+let someNumber=33
+let strNum=String(someNumber)
+console.log(strNum)
+console.log(typeof strNum)
+
