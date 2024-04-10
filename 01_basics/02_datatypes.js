47de6c8b8b36c4393fe3192739c23b09c1d7f45a
shubhi-D <115149360+shubhi-D@users.noreply.github.com>
2024-04-10 18:50:57 +0000

    code for datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using node.js not browser
+
+console.log(3+
+    3)  // code readability should be high 
+
+console.log("shubhi")
+
+let name="shubhi"
+let age=21
+let isLoggedIn=false
+let state;
+
+/* 
+PRIMITIVE DATA-TYPES:-
+   number=> 2^53
+   bigint
+   string=>""
+   boolean=>true/false
+   null =>representation of empty value :(this is stand-alone value)
+   undefined=> when value is not assigned
+   symbol => unique
+*/
+console.log("type of age is :-")
+console.log(typeof age);
+console.log("type of NULL is:-");
+console.log(typeof  null); //object
