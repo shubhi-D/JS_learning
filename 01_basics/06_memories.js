4a2434fcbb6ae19d130f3b58c590ac178b504204
shubhi-D <115149360+shubhi-D@users.noreply.github.com>
2024-04-11 03:51:29 +0000

    memory: heap & stack

--- 01_basics/06_memories.js
+++ 01_basics/06_memories.js
@@ -1,6 +1,6 @@
 /* two types of memory-
-     stack(primitive)
-     heap(non-premitive)
+     stack(primitive): like Number, String : we geta copy
+     heap(non-premitive): we get refrence
  */
 
      let myName="kalimbaGrooves"
@@ -9,4 +9,19 @@
      anotherName="shubhi-D"
      console.log(myName)
      console.log(anotherName)
-     console.log(anotherName)
+     //console.log(anotherName)
+
+     let userOne={
+        email:"shubhyuvrajsharma001@gmail.com",
+        upiNum:"shubhi"
+     }
+
+     let userTwo=userOne
+
+     userTwo.email="uv"
+     userOne.email="shubha"
+
+     console.log(userOne.email)//shubha
+     console.log(userTwo.email)//shubha
+
+     //this means latest change is printed....
