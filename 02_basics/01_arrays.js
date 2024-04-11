f46d6c15479f8943cb3f186f53fde5522c641765
shubhi-D <115149360+shubhi-D@users.noreply.github.com>
2024-04-11 16:34:59 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+//Array: in js it is resizeable, copy-operation: shallow copy: whose property is shared by same refrence point.(as in heap)
+
+const arr=[0,3,2,9]
+const radha=["kishori","nitya","dhriti","gamya"]
+const arr2=new Array(1,2,3,4,5)
+console.log(arr[0])
+console.log(radha[2])
+
+//METHODS IN ARRAY
+
+arr.push(6)
+arr.push(10)
+console.log(arr)
+
+
